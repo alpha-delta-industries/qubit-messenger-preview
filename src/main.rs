f0851f19b7f50623bbaef84745e7a8a40c6753e5
SeniorShadifer <discordeg@mail.ru>
2026-02-06 19:31:36 +0300

    Logger initializing moved to special function.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 mod cli;
+mod logging;
 
 use clap::Parser;
-use log::trace;
+use log::{debug, trace};
 
 use crate::cli::{CLIArguments, Commands};
 
 fn main() {
-    pretty_env_logger::init();
+    logging::init_logger();
     trace!("Logger initialized.");
 
     let cli_args = CLIArguments::parse();
-    trace!("CLI agruments parsed successfully: {:?}", cli_args);
+    debug!("CLI agruments parsed successfully: {:?}", cli_args);
 
     match &cli_args.command {
         Some(Commands::Test { echo_str }) => {
