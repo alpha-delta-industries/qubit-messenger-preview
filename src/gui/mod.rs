0c97f15827dc5f38cb9f7f91f692e96c44350095
SeniorShadifer <discordeg@mail.ru>
2026-02-08 12:28:36 +0300

    Backup 08.02.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,5 +1,28 @@
-use iced::widget::{button, column, text};
-use iced::{Element, Theme};
+use iced::alignment::Vertical;
+use iced::widget::{Button, Space, Svg, Text, button, column, container, row, text};
+use iced::{Element, Length, Theme};
+
+macro_rules! button_with_icon {
+    ($icon_path:literal, $button_text:literal) => {
+        Button::new(
+            row![
+                Svg::from_path($icon_path).height(25).width(25),
+                Space::new().width(10),
+                Text::new($button_text),
+            ]
+            .align_y(Vertical::Center),
+        )
+    };
+}
+
+macro_rules! left_nav_button {
+    ($icon_path:literal, $button_text:literal, $message:expr) => {
+        button_with_icon!($icon_path, $button_text)
+            .on_press($message)
+            .width(175)
+            .style(button::secondary)
+    };
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Message {
@@ -23,7 +46,10 @@ impl Page {
     pub fn view(&self) -> Element<'static, Message> {
         match self {
             Self::Home => text!("Home page content.").into(),
-            Self::Test => text!("Test page content.").into(),
+            Self::Test => Svg::from_path("./assets/icons/activity.svg")
+                .height(100)
+                .width(100)
+                .into(),
         }
     }
 }
@@ -46,18 +72,41 @@ impl App {
     }
 
     fn view(&self) -> Element<'_, Message> {
-        column![
-            self.page.view(),
-            button("Go home").on_press(Message::GoToHomePage),
-            button("Go test").on_press(Message::GoToTestPage),
-        ]
-        .spacing(10)
+        container(
+            row![
+                container(
+                    column![
+                        left_nav_button!(
+                            "assets/icons/activity.svg",
+                            "Home",
+                            Message::GoToHomePage
+                        ),
+                        left_nav_button!(
+                            "assets/icons/activity.svg",
+                            "Test",
+                            Message::GoToTestPage
+                        )
+                    ]
+                    .spacing(2)
+                )
+                .width(200)
+                .align_y(Vertical::Center),
+                container(self.page.view()).width(Length::Fill)
+            ]
+            .width(Length::Fill)
+            .height(Length::Fill),
+        )
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .align_y(Vertical::Center)
         .into()
     }
 
     pub fn run() -> iced::Result {
         iced::application(App::new, App::update, App::view)
             .theme(Theme::Dark)
+            .title("Untitled P2P messenger")
+            .window_size((500, 400))
             .run()
     }
 }
